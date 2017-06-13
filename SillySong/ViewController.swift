5bd5e689c358e5e3a361dcdbddf960eb6d21d256
adjustnox <derek@noxsolutions.com>
2017-06-12 21:01:02 -0400

    complete project

--- SillySong/ViewController.swift
+++ SillySong/ViewController.swift
@@ -1,25 +1,90 @@
 //
 //  ViewController.swift
-//  SillySong
+//  Silly Songs
 //
 //  Created by Derek Justus on 6/12/17.
-//
+//  Copyright © 2017 Nox Solutions. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var lyricsTextView: UITextView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        nameTextField.delegate = self;
+        
+        
+        print(lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: "Nate"))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func reset(_ sender: Any) {
+        nameTextField.text = ""
+        lyricsTextView.text = ""
     }
-
-
+    @IBAction func displayLyrics(_ sender: Any) {
+        if !(nameTextField.text?.isEmpty)! {
+           lyricsTextView.text = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: nameTextField.text!)
+        }
+    }
+   
+    
+    let bananaFanaTemplate = [
+        "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+        "Banana Fana Fo F<SHORT_NAME>",
+        "Me My Mo M<SHORT_NAME>",
+        "<FULL_NAME>"].joined(separator: "\n")
+    
+   
+    
+    
+}
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
 }
 
+
+func shortNamefromName(name: String) -> String {
+    
+    var nameToReturn = name.lowercased()
+    
+    let chars = nameToReturn.characters
+    
+    for letter in chars {
+        switch letter {
+        case "a", "e", "i", "o", "u":
+            
+            let currentIndex = nameToReturn.index(nameToReturn.startIndex, offsetBy: 0)
+            nameToReturn = nameToReturn.substring(from: currentIndex)
+            
+            return nameToReturn
+            
+        default:
+            let currentIndex = nameToReturn.index(nameToReturn.startIndex, offsetBy: 1)
+            nameToReturn = nameToReturn.substring(from: currentIndex)
+            
+            
+        }
+        
+    }
+    
+    return nameToReturn
+}
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    
+    let replacedString = lyricsTemplate.replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+        .replacingOccurrences(of: "<SHORT_NAME>", with: shortNamefromName(name: fullName))
+    
+    return replacedString
+}
